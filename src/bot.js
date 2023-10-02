d4c5594f53b12eabc317ed01b4e520edb8461f2c
Filipe Mota <nada.geral@gmail.com>
2023-10-03 00:06:46 +0100

    error handling and error msg

--- src/bot.js
+++ src/bot.js
@@ -1,7 +1,15 @@
-const { Client, GatewayIntentBits, Events, quote } = require('discord.js');
-const fetch = require('node-fetch');
-const translate = require('google-translate-api-x');
+const {
+    Client,
+    GatewayIntentBits,
+    Events,
+    Partials,
+    ChannelType,
+    quote,
+} = require("discord.js");
+const fetch = require("node-fetch");
+const translate = require("google-translate-api-x");
 const countries = require("./countries");
+const fetchBuilderOptions = require("./fetch-builder");
 
 const intents = [
     GatewayIntentBits.Guilds,
@@ -9,43 +17,75 @@ const intents = [
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.GuildMessageReactions,
     GatewayIntentBits.MessageContent,
+    GatewayIntentBits.DirectMessages,
 ];
+const partials = [Partials.Message, Partials.Channel, Partials.Reaction];
 
-const client = new Client({ intents });
+const client = new Client({ intents, partials });
 const token = process.env.discord_key;
+
+client.on(Events.MessageCreate, async (message) => {
+    if (message.author.bot) return; 
+   
+    if (message.channel.type === ChannelType.DM) 
+        message.author.send("Hi, thanks for sending me a private message. I appreciate your interest to speak with me. However, I'm not able to respond to questions, comments or information. Please use the appropriate channel on the server to communicate with your colleagues. Thank you for your understanding and cooperation."); 
+});
 
 client.on(Events.MessageReactionAdd, async (reaction, user) => {
     if (user.bot) return;
-    if (typeof reaction.emoji.name !== 'string') return;
+    if (typeof reaction.emoji.name !== "string") return;
+
+    if (reaction.partial) {
+        try {
+            await reaction.fetch();
+        } catch (error) {
+            console.error("Something went wrong when fetching the message:", error);
+            user.send(
+                quote(messageToTranslate) +
+                " \n\n " +
+                `Error: It is no longer possible to translate this message` +
+                " \n\n "
+            );
+            return;
+        }
+    }
 
     const reactionName = reaction.emoji.name;
     const countryInformation = countries[reactionName];
 
     if (countryInformation === undefined || countryInformation === null) {
-        user.send(`Error: The country ${reactionName} is not available for translation \n Please, try again with another country \n\n`);
+        user.send(
+            `Error: The country ${reactionName} is not available for translation \n Please, try again with another country \n\n`
+        );
         return;
-    };
+    }
 
     const messageToTranslate = reaction.message.content;
 
-    if (messageToTranslate === undefined || messageToTranslate === null || messageToTranslate === "") return;
+    if (
+        messageToTranslate === undefined ||
+        messageToTranslate === null ||
+        messageToTranslate === ""
+    )
+        return;
 
     await translate(messageToTranslate, {
         to: countryInformation.code,
         forceBatch: false,
         autoCorrect: true,
-        requestFunction: fetch
+        requestFunction: fetch,
     })
-        .then(res => user.send(quote(messageToTranslate)
-            + " \n\n "
-            + res.text
-            + " \n\n "
-        ))
-        .catch(() => user.send(quote(messageToTranslate)
-            + " \n\n "
-            + `Error: It is not possible to translate into language for the country ${countryInformation.name}`
-            + " \n\n "
-        ));
+        .then((res) =>
+            user.send(quote(messageToTranslate) + " \n\n " + res.text + " \n\n ")
+        )
+        .catch(() =>
+            user.send(
+                quote(messageToTranslate) +
+                " \n\n " +
+                `Error: It is not possible to translate into language for the country ${countryInformation.name}` +
+                " \n\n "
+            )
+        );
 });
 
 client.login(token);
