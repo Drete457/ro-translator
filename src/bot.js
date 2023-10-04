42985d13978a90ed8c4393d2317439168b27dab0
Filipe Mota <nada.geral@gmail.com>
2023-10-04 03:41:22 +0100

    add character ai

--- src/bot.js
+++ src/bot.js
@@ -9,6 +9,7 @@ const {
 const fetch = require("node-fetch");
 const translate = require("google-translate-api-x");
 const countries = require("./countries");
+const { init, chat, characterAiActive } = require("./characterai");
 
 const intents = [
     GatewayIntentBits.Guilds,
@@ -26,8 +27,16 @@ const token = process.env.discord_key;
 client.on(Events.MessageCreate, async (message) => {
     if (message.author.bot) return;
 
-    if (message.channel.type === ChannelType.DM)
-        message.author.send("Hi, thanks for sending me a private message. I appreciate your interest to speak with me. However, I'm not able to respond to questions, comments or information. Please use the appropriate channel on the server to communicate with your colleagues. Thank you for your understanding and cooperation.");
+    if (message.channel.type === ChannelType.DM) {
+        if (characterAiActive) {
+            chat(message.content)
+                .then((res) => message.author.send(res))
+                .catch(() => message.author.send("Hi, thanks for sending me a private message. I appreciate your interest to speak with me. However, I'm not able to respond right now, try later. Thank you for your understanding and cooperation."));
+            return;
+        }
+    }
+
+    message.author.send("Hi, thanks for sending me a private message. I appreciate your interest to speak with me. However, I'm not able to respond to questions, comments or information at the moment, my Ai is disable for now. Please use the appropriate channel on the server to communicate with your colleagues. Thank you for your understanding and cooperation.");
 });
 
 client.on(Events.MessageReactionAdd, async (reaction, user) => {
@@ -88,3 +97,4 @@ client.on(Events.MessageReactionAdd, async (reaction, user) => {
 });
 
 client.login(token);
+init();
---
+++ src/characterai.js
@@ -0,0 +1,26 @@
+const CharacterAI = require("node_characterai");
+const characterAI = new CharacterAI();
+
+const characterId = process.env.characterId;
+const access_Token = process.env.access_Token_ai;
+let characterAiActive = false;
+let chat;
+
+const characterAiInit = async () => {
+    await characterAI.authenticateWithToken(access_Token).then(() => console.log("CharacterAi Authenticated with token")).catch(() => console.log("Error authenticating with token"));
+    
+    await characterAI.createOrContinueChat(characterId).then((resp) => {
+        console.log("CharacterAi chat created or continued");
+        characterAiActive = true
+        chat = resp;
+    }).catch(() => console.log("Error creating or continuing chat"));
+
+    console.log("CharacterAi init finished, is active? ", characterAiActive);
+};
+
+const characterAiChat = async (message) => {
+    const response = await chat.sendAndAwaitResponse(message, true);
+    return response.text;
+};
+
+module.exports = { init: characterAiInit, chat: characterAiChat, characterAiActive };
