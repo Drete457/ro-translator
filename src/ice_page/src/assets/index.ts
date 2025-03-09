e3014d517bed743070149ecd89e6b1bd214e5e8c
Filipe Mota <nada.geral@gmail.com>
2025-03-09 23:02:12 +0000

    add small logo

--- src/ice_page/src/assets/index.ts
+++ src/ice_page/src/assets/index.ts
@@ -1,6 +1,6 @@
 import leagueOfOrder from './league_of_order.jpg';
 import springwardens from './springwardens.jpg';
 import wilderburg from './wilderburg.jpg';
-import logo from './logo.png';
+import logo from './logo.jpg';
 
 export { leagueOfOrder, springwardens, wilderburg, logo };
